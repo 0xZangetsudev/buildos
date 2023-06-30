549da8a19e31cbc267875709c8841c7c94e33f8e
0xZangetsueth <moraleswill@protonmail.com>
2023-06-30 08:47:55 +0200

    working with registers

--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,28 @@
 org 0x7C00
 bits 16
 
+start:
+    jmp main
+
+puts:
+    ; save registers
+    push si 
+    push ax
+
+.loop:
+    lodsb  ;loadsnext character in al
+    or al, al       ;verify if next character is null
+
 main: 
+    ;setup data segments
+    mov ax, 0    ;can't write to ds/es directly
+    mov ds, ax
+    mov es, ax
+
+    ;setup stack
+    mov ss, ax
+    mov sp, 0x7C00
+
     hlt
 
 .halt:
